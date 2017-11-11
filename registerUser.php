b64dfc6dd35a6e7b9ca698c023b60ba96e9b265f
Rodrigo R. Galvão <rodrigorosattig@gmail.com>
2017-11-11 15:05:07 -0200

    Make some major changes
    1. Login works
    	1.1. It checks whether the user is a client or an admin.
    2. Admin options:
    	2.1. Add/Update client: add is working, but update is not
    working yet.
    	2.2. Show Clients: working!
    	2.3. Logout: not implemented!
    
    TODO:
    1. Make Update works.
    2. Implement Logout functionality.
    3. Use CSS to make some pages look better.
    4. Implement Client functionalities.
    
    Signed-off-by: Rodrigo R. Galvão <rodrigorosattig@gmail.com>

--- registerUser.php
+++ registerUser.php
@@ -1,11 +1,12 @@
 <?php
+
 	try{
 		$user = "root";
 		$pass = "";
 		
 		$firstname= $_POST['firstname'];
 		$lastname= $_POST['lastname'];
-		$user = $_POST['user'];
+		$username = $_POST['user'];
 		$password = $_POST['password'];
 		$type = $_POST['type'];
 		$birthday= $_POST['birthday'];
@@ -19,19 +20,21 @@
 		$cep= $_POST['cep'];
 		$country= $_POST['country'];
 		
-		$conn = new PDO("mysql:host=localhost;dbname=crud_phpset", $user, $pass);
+		$conn = new PDO("mysql:host=localhost;dbname=crud_PHPSet", $user, $pass);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 		
-		$query = "SELECT COUNT(*) from user where cpf='".$cpf."'";
+		$query = "SELECT * FROM user where cpf='".$cpf."'";
 		$result = $conn->query($query);
 		
 		$sql = "";
 		
-		if($result->rowcount() > 0){
-			$sql = "UPDATE user SET firstname = '".$firstname."', lastname = '".$lastname."' , user = '".$user."', password = '".$password."' ,  type= '".$type."', birthday = '".$birthday."' , phone = '".$phone."', email = '".$email."', cpf = '".$cpf."', street = '".$street."', number = '".$number."', complement = '".$complement."', city = '".$city."', cep = '".$cep."', country = '".$country."' WHERE cpf = '".$cpf."'";
+		if($result->rowCount() > 0){
+			$sql = "UPDATE user SET firstname = '".$firstname."', lastname = '".$lastname."', user = '".$username."', password = '".$password."', type = '".$type."', birthday = '".$birthday."', phone = '".$phone."', email = '".$email."', cpf = '".$cpf."', street = '".$street."', number = '".$number."', complement = '".$complement."', city = '".$city."', cep = '".$cep."', country = '".$country."' WHERE cpf = '".$cpf."'";
+			echo "<br/>".$sql."<br/>";
 			$conn->exec(sql);
 		}else {			
-			$sql = "INSERT INTO user VALUES ('".$firstname."','".$lastname."','".$user."','".$password."','".$type."','".$birthday."','".$cpf."','".$phone."','".$email."','".$cpf."','".$street."', '".$number."', '".$complement."','".$city."','".$cep."', '".$country."')";
+			$sql = "INSERT INTO user VALUES ('".$firstname."','".$lastname."','".$username."','".$password."','".$type."','".$birthday."','".$cpf."','".$phone."','".$email."','".$street."', '".$number."', '".$complement."','".$city."','".$cep."', '".$country."')";
+			//echo "<br/>".$sql."<br/>";
 			$conn->exec($sql);
 		}
 	}
